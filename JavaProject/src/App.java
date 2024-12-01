8474648e4186c190c736dd6c1143ec6543b6d65a
Alex <akutofaction@gmail.com>
2024-12-01 10:08:30 -0300

    Git init push

---
+++ JavaProject/src/App.java
@@ -0,0 +1,10 @@
+public class App {
+    
+    public static void main(String[] args) throws Exception {
+        
+        for (int i=0; i < 100; i++) {
+            for (int f=0; f<1000;f++)
+                {System.out.println(f+" "+i);}
+        }
+    }
+}
