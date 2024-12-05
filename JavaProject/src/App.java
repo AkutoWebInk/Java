430e35fdba91340e8dd84d25ab6ae4f23ddb5f58
Alex <akutofaction@gmail.com>
2024-12-05 09:01:31 -0300

    Understanding Static and non- Static classes/functions access

--- JavaProject/src/App.java
+++ JavaProject/src/App.java
@@ -1,10 +1,20 @@
 public class App {
     
-    public static void main(String[] args) throws Exception {
-        
-        for (int i=0; i < 100; i++) {
-            for (int f=0; f<1000;f++)
-                {System.out.println(f+" "+i);}
-        }
+public static void main (String[]args){
+    
+    boolean numero = true;
+    int a = 2;
+    App test = new App();
+    
+    test.testPrintIn(numero, a);
     }
-}
+
+    public void testPrintIn(boolean numero, int a)
+    {
+        if(numero && a==2){
+            System.out.println(numero);}
+        else{
+            System.out.println("Error" + "!= && =!");}
+    }
+
+}   
